8a2f97c7880ce1fc198a7f927d94bbb610a0b6bb
yashprince123456 <kothamdi.yash17@libertycommon.org>
2016-02-23 19:08:13 -0700

    Added files via upload

---
+++ fib.c
@@ -0,0 +1,22 @@
+int main()
+{
+   int n, first = 0, second = 1, next, c;
+ 
+   printf("n= ?");
+   scanf("%d",&n);
+ 
+   for ( c = 0 ; c < n ; c++ )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      printf("%d\n",next);
+   }
+ 
+   return 0;
+}
